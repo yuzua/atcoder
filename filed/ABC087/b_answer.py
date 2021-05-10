81c39083fe4a4d1f2c50226cfaf5c557b4e5750d
ゆずき <k.saito70329@gmail.com>
2021-05-10 14:13:11 +0900

    [update] ABC087

--- filed/ABC087/b_answer.py
+++ filed/ABC087/b_answer.py
@@ -1,6 +1,8 @@
 # for文を用いた全探索 多重for文
 
+# 1
 A, B, C, X = [int(input()) for _ in range(4)]
 print([500*a + 100*b + 50*c for c in range(C+1) for b in range(B+1) for a in range(A+1)].count(X))
 
+# 2
 a,b,c,x=eval('int(input())+1,'*4);print(sum(c>x/50-I//b*10-I%b*2>0for I in range(a*b)))
