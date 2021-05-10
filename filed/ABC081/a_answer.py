0efcfbf1dddfc05a1fc05d15ef66e424377f32a0
ゆずき <k.saito70329@gmail.com>
2021-05-10 12:32:33 +0900

    [update] ABC081

--- filed/ABC081/a_answer.py
+++ filed/ABC081/a_answer.py
@@ -1,5 +1,7 @@
 # for文を使わない程度の全探索 string型
 
+# 1
 print(int(input())%9)
 
+# 2
 print(input().count("1"))
--- filed/ABC081/b_answer.py
+++ filed/ABC081/b_answer.py
@@ -1,8 +1,10 @@
 # for文を用いた全探索 シュミレーション
 
+# 1
 n=eval([*open(0)][1].replace(*' |'))
 print(len(bin(n&-n))-3)
 
+# 2
 input()
 n=eval(input().replace(' ','|'))
 print(len(bin(n&-n))-3)
