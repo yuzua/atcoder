abf9644bf37a01d4f67155f9aac61b272ac585d7
ゆずき <k.saito70329@gmail.com>
2021-05-28 11:10:45 +0900

    [update] ABC133

---
+++ filed/ABC133/b.py
@@ -0,0 +1,12 @@
+N, D = map(int, input().split())
+df = []
+
+for n in range(N):
+    df_sample = []
+    for d in range(D):
+        df_sample.append(input())
+    df.append(df_sample)
+
+for i in range(N):
+    # 途中
+    pass
